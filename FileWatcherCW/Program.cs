67ff157271f2418cc96a2f2934ebc92165697df8
dimitar.nedilkovski@gmail.com <dimitar.nedilkovski@gmail.com>
2019-10-19 14:22:36 +0300

    Create code signature

--- FileWatcherCW/Program.cs
+++ FileWatcherCW/Program.cs
@@ -8,12 +8,30 @@ namespace FileWatcherCW
 {
     class FileWatcher
     {
+        public FileWatcher()
+        {
 
+        }
+        public void Start()
+        {
+            while (true)
+            {
+
+            }
+        }
+        public event EventHandler Check;
     }
     class Program
     {
         static void Main(string[] args)
         {
+            FileWatcher w = new FileWatcher();
+            w.Check += W_Check;
+        }
+
+        private static void W_Check(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
