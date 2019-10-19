23f5613a77cf109dd97a3a4bc3e1ebac70caf78a
dimitar.nedilkovski@gmail.com <dimitar.nedilkovski@gmail.com>
2019-10-19 15:22:27 +0300

    Change task invoke method and  add event code when locker  synchronize tasks

--- FileWatcherCW/Program.cs
+++ FileWatcherCW/Program.cs
@@ -1,37 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FileWatcherCW
 {
     class FileWatcher
     {
-        public FileWatcher()
+        private object _lock;
+        private string _path;
+        public DateTime lastTime;
+        public DateTime checkTime;
+        public FileWatcher(string path)
         {
-
+            _path = path;
+            lastTime = File.GetLastWriteTime(_path);
         }
         public void Start()
         {
+
             while (true)
             {
 
+                checkTime = File.GetLastWriteTime(_path);
+
+                if (lastTime != checkTime)
+                {
+                    lastTime = File.GetLastWriteTime(_path);
+                    Task.Run(() => Check?.Invoke());
+                }
             }
         }
-        public event EventHandler Check;
+        public event Action Check;
+
     }
     class Program
     {
+        private static object _lock = new object();
         static void Main(string[] args)
         {
-            FileWatcher w = new FileWatcher();
-            w.Check += W_Check;
+            File.Create("1.txt").Dispose();
+            FileWatcher w = new FileWatcher("1.txt");
+            w.Check += Check;
+            Task.Run(() => w.Start());
+
+
+            while (true)
+            {
+                lock (_lock)
+                {
+                    Console.WriteLine("Do u wanna change ur text file: 1.txt");
+                }
+                    var res = Console.ReadLine();
+                
+                if (res == "yes")
+                {
+
+                    lock (_lock)
+                    {
+                        File.WriteAllText("1.txt", "1");
+                    }
+                }
+                Console.WriteLine("Wait...");
+            }
         }
 
-        private static void W_Check(object sender, EventArgs e)
+        private static void Check()
         {
-            throw new NotImplementedException();
+
+           
+
+            lock (_lock)
+            {
+                string contains = File.ReadAllText("1.txt");
+                if (contains == "1")
+                {
+
+                    File.WriteAllText("1.txt", "0");
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Correct operation, '0' in 1.txt");
+
+
+                }
+            }
         }
     }
 }
+
